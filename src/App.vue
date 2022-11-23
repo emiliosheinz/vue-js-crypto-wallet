03e81eb5e2e59c66439cea5274a9a113f1939dcb
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-11-23 14:37:56 -0300

    chore: :recycle: cleaning project structure

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router';
-import HelloWorld from './components/HelloWorld.vue';
 </script>
 
 <template>
@@ -14,11 +13,9 @@ import HelloWorld from './components/HelloWorld.vue';
     />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/wallet">Wallet</RouterLink>
       </nav>
     </div>
   </header>
