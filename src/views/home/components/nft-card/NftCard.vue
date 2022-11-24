2234cdee0aad86e15b8d03d14f31bb28d4ec9fda
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-11-24 18:13:21 -0300

    style: :lipstick: updating minor style on NftCard

--- src/views/home/components/nft-card/NftCard.vue
+++ src/views/home/components/nft-card/NftCard.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 	import ButtonComponent from '@/components/button/ButtonComponent.vue'
+	import EthereumPrice from '@/components/ethereum-price/EthereumPrice.vue'
 	import type { Nft } from '@/types/nft'
 
 	defineProps<{
@@ -10,24 +11,15 @@
 </script>
 
 <template>
-	<div class="w-80 rounded-3xl overflow-hidden bg-white">
-		<img :src="nft.image.url" class="w-full h-80" />
+	<div class="w-full md:w-80 rounded-3xl overflow-hidden bg-white">
+		<img :src="nft.image.url" class="w-full h-80 object-cover" />
 		<div class="space-y-2 p-3">
 			<div class="flex flex-row">
 				<div class="flex flex-1 flex-col">
 					<p class="line-clamp-1 font-bold">{{ nft.collection.name }}</p>
 					<p class="font-light">#{{ nft.id }}</p>
 				</div>
-				<div class="flex items-start gap-2">
-					<img
-						alt="Ethereum coin symbol"
-						src="@/assets/ethereum.svg"
-						width="9"
-						height="15"
-						class="pt-1"
-					/>
-					{{ nft.price }}
-				</div>
+				<EthereumPrice :price="nft.price" />
 			</div>
 			<ButtonComponent
 				:isDisabled="isBuyButtonDisable"
