326093dc6ffd74241af4c78974738f878c343b4d
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-11-24 18:17:15 -0300

    feat: :sparkles: creating wallet summary component

---
+++ src/views/wallet/components/summary/SummaryComponent.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+	import EthereumPrice from '@/components/ethereum-price/EthereumPrice.vue'
+	import { ethereumToDollar } from '@/utils/currency'
+	import type { Nft } from '@/types/nft'
+
+	defineProps<{ nfts: Nft[]; totalAmountInWallet: number }>()
+</script>
+
+<template>
+	<section class="h-min bg-white py-6 px-8 w-full rounded-3xl lg:w-96">
+		<table class="w-full">
+			<thead>
+				<tr>
+					<td><h2 class="font-bold text-2xl">Summary</h2></td>
+				</tr>
+			</thead>
+			<tbody>
+				<tr v-for="nft in nfts" :key="`summary-${nft.id}`">
+					<td>
+						<p class="font-light">#{{ nft.id }}</p>
+					</td>
+					<td><EthereumPrice :price="nft.price" /></td>
+					<td class="flex justify-end">
+						<p class="font-light">{{ ethereumToDollar(nft.price) }}</p>
+					</td>
+				</tr>
+			</tbody>
+			<tfoot>
+				<tr>
+					<td><p class="font-bold text-xl">Total</p></td>
+					<td>
+						<EthereumPrice :price="totalAmountInWallet" />
+					</td>
+					<td class="flex justify-end">
+						{{ ethereumToDollar(totalAmountInWallet) }}
+					</td>
+				</tr>
+			</tfoot>
+		</table>
+	</section>
+</template>
