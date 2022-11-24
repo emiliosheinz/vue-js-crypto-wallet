11ff03901b0566f838b097b680ad324aee631e40
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-11-24 18:16:53 -0300

    feat: :sparkles: creating wallet nft card

---
+++ src/views/wallet/components/nft-card/NftCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+	import EthereumPrice from '@/components/ethereum-price/EthereumPrice.vue'
+	import type { Nft } from '@/types/nft'
+
+	defineProps<{ nft: Nft }>()
+</script>
+
+<template>
+	<div class="flex p-5 rounded-3xl bg-white space-x-5">
+		<img :src="nft.image.url" class="w-24 h-24 rounded-xl" />
+		<div class="flex flex-col justify-between">
+			<div>
+				<p class="line-clamp-1 font-bold">{{ nft.collection.name }}</p>
+				<p class="font-light">#{{ nft.id }}</p>
+			</div>
+			<EthereumPrice :price="nft.price" />
+		</div>
+		<div></div>
+	</div>
+</template>
