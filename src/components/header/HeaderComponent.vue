cd66d74055078d9cac30b09ad530bc8732169c50
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-11-24 07:42:12 -0300

    feat: :sparkles: adds data-testids and store connection on header

--- src/components/header/HeaderComponent.vue
+++ src/components/header/HeaderComponent.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 	import { RouterLink } from 'vue-router'
+	import { useWalletStore } from '@/stores/wallet'
+
+	const wallet = useWalletStore()
 </script>
 
 <template>
 	<header class="max-w-6xl w-full p-5 m-auto flex justify-between">
-		<RouterLink to="/">
+		<RouterLink to="/" data-testid="home-router-link">
 			<img
 				alt="Crypto Store logo"
 				src="@/assets/logo.svg"
@@ -13,11 +16,11 @@
 			/>
 		</RouterLink>
 
-		<RouterLink to="/wallet">
+		<RouterLink to="/wallet" data-testid="wallet-router-link">
 			<div class="flex space-x-1">
 				<div class="flex flex-col items-end text-white justify-start">
 					<p class="font-bold text-base">My Wallet</p>
-					<p class="font-normal text-sm -mt-1">1 item</p>
+					<p class="font-normal text-sm -mt-1">{{ wallet.nfts.length }} item</p>
 				</div>
 				<img
 					alt="Vue logo"
