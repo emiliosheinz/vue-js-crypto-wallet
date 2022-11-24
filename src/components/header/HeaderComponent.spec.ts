eecaa27988eed72bea23656698b401f907230e25
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-11-24 07:41:36 -0300

    test: :white_check_mark: adding headers tests

--- src/components/header/HeaderComponent.spec.ts
+++ src/components/header/HeaderComponent.spec.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, test } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import HeaderComponent from './HeaderComponent.vue'
@@ -25,4 +25,37 @@ describe('HeaderComponent', () => {
 
 		expect(component.html().startsWith('<header')).toBe(true)
 	})
+
+	it('should render a router link for home', () => {
+		const component = mount(HeaderComponent, {
+			global: {
+				plugins: [router],
+			},
+		})
+
+		const link = component.find('[data-testid="home-router-link"]')
+
+		expect(link).toBeTruthy()
+		expect(link.html().startsWith('<a')).toBe(true)
+		expect(link.attributes().href).toEqual('/')
+	})
+
+	it('should render a router link for wallet', () => {
+		const component = mount(HeaderComponent, {
+			global: {
+				plugins: [router],
+			},
+		})
+
+		const link = component.find('[data-testid="wallet-router-link"]')
+
+		expect(link).toBeTruthy()
+		expect(link.html().startsWith('<a')).toBe(true)
+		expect(link.attributes().href).toEqual('/wallet')
+	})
+
+	test.todo('should render the correct amount of items in the wallet')
+	test.todo('should show items when there is no item in the wallet')
+	test.todo('should show item when there is only one item in the wallet')
+	test.todo('should show items when there is more then one item in the wallet')
 })
