8b39d1bbed7dfc4f141c79f08102cf355a381ab0
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-11-24 18:12:32 -0300

    feat: :sparkles: creating Ethereum price component

---
+++ src/components/ethereum-price/EthereumPrice.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+	import type { Nft } from '@/types/nft'
+
+	defineProps<{ price: Nft['price'] }>()
+</script>
+
+<template>
+	<div class="flex items-start gap-2">
+		<img
+			alt="Ethereum coin symbol"
+			src="@/assets/ethereum.svg"
+			width="9"
+			height="15"
+			class="pt-1"
+		/>
+		{{ price }}
+	</div>
+</template>
