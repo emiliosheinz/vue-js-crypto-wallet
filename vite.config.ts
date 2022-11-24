c84f73383410622a423383ff045511ecefbc334b
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-11-24 07:40:45 -0300

    feat: :wrench: adding coverage and tsconfig files

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -9,6 +10,11 @@ export default defineConfig({
 	resolve: {
 		alias: {
 			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	test: {
+		coverage: {
+			provider: 'istanbul',
 		},
 	},
 })
