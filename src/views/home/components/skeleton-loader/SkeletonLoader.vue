0b8950b1fea2ca8ae58d7e4c458d85ee24ce9e5e
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-11-24 18:16:35 -0300

    feat: :sparkles: creating skeleton loader

---
+++ src/views/home/components/skeleton-loader/SkeletonLoader.vue
@@ -0,0 +1,24 @@
+<template>
+	<ul
+		class="pb-10 px-5 md:px-0 flex md:justify-center flex-wrap gap-4 md:gap-8 animate-pulse"
+	>
+		<li
+			class="h-96 w-full md:w-80 rounded-3xl overflow-hidden bg-slate-800"
+		></li>
+		<li
+			class="h-96 w-full md:w-80 rounded-3xl overflow-hidden bg-slate-800"
+		></li>
+		<li
+			class="h-96 w-full md:w-80 rounded-3xl overflow-hidden bg-slate-800"
+		></li>
+		<li
+			class="h-96 w-full md:w-80 rounded-3xl overflow-hidden bg-slate-800"
+		></li>
+		<li
+			class="h-96 w-full md:w-80 rounded-3xl overflow-hidden bg-slate-800"
+		></li>
+		<li
+			class="h-96 w-full md:w-80 rounded-3xl overflow-hidden bg-slate-800"
+		></li>
+	</ul>
+</template>
