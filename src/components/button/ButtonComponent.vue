c9797906d07b4c917e4f9e3fa2ce67805bf3fb94
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-11-24 07:42:28 -0300

    feat: :sparkles: adds new button props

--- src/components/button/ButtonComponent.vue
+++ src/components/button/ButtonComponent.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-	defineProps<{ label: string }>()
+	defineProps<{ label: string; onClick: () => void; isDisabled?: boolean }>()
 </script>
 
 <template>
-	<button class="w-full h-10 bg-slate-900 text-white rounded-3xl shadow-md">
+	<button
+		type="button"
+		:disabled="isDisabled"
+		@click="onClick"
+		class="w-full h-10 bg-slate-900 text-white rounded-3xl shadow-md disabled:opacity-50"
+	>
 		{{ label }}
 	</button>
 </template>
