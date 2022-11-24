9155489f5174c530f47be3e9c882ef00af75e154
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-11-24 18:14:47 -0300

    feat: :sparkles: adding totalAmountInWallet getter to store

--- src/stores/wallet.ts
+++ src/stores/wallet.ts
@@ -9,6 +9,12 @@ export const useWalletStore = defineStore('wallet', {
 	state: (): WalletStore => {
 		return { nfts: [] }
 	},
+	getters: {
+		totalAmountInWallet: state => {
+			const total = state.nfts.reduce((acc, nft) => acc + nft.price, 0)
+			return Number(total.toFixed(2))
+		},
+	},
 	actions: {
 		addNft(nft: Nft) {
 			const hasNftInWallet = this.nfts.some(({ id }) => id === nft.id)
