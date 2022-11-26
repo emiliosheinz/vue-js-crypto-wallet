0459284b091500831328805c4709cad54ebc23e6
Emilio Heinzmann <emiliosheinz@gmail.com>
2022-11-25 22:17:48 -0300

    test: :white_check_mark: adds tests for trash cann

--- src/views/wallet/components/nft-card/NftCard.spec.ts
+++ src/views/wallet/components/nft-card/NftCard.spec.ts
@@ -1,13 +1,16 @@
 import { makeNft } from '@/utils/tests'
 import NftCard from './NftCard.vue'
 import { mount } from '@vue/test-utils'
-import { describe, expect, it } from 'vitest'
+import { describe, expect, it, vi } from 'vitest'
+import type { Nft } from '@/types/nft'
 
 describe('Wallet NftCard', () => {
 	it('should render nft image', () => {
 		const nft = makeNft()
 
-		const component = mount(NftCard, { props: { nft } })
+		const component = mount(NftCard, {
+			props: { nft, onDeletePress: () => undefined },
+		})
 		const nftImage = component.find('[data-testid="nft-image"]')
 
 		expect(nftImage.attributes().src).toBe(nft.image.url)
@@ -16,7 +19,9 @@ describe('Wallet NftCard', () => {
 	it('should render nft collection name', () => {
 		const nft = makeNft()
 
-		const component = mount(NftCard, { props: { nft } })
+		const component = mount(NftCard, {
+			props: { nft, onDeletePress: () => undefined },
+		})
 		const nftImage = component.find('[data-testid="nft-collection-name"]')
 
 		expect(nftImage.text()).toBe(nft.collection.name)
@@ -25,7 +30,9 @@ describe('Wallet NftCard', () => {
 	it('should render nft id with # prefix', () => {
 		const nft = makeNft()
 
-		const component = mount(NftCard, { props: { nft } })
+		const component = mount(NftCard, {
+			props: { nft, onDeletePress: () => undefined },
+		})
 		const nftId = component.find('[data-testid="nft-id"]')
 
 		expect(nftId.text()).toBe(`#${nft.id}`)
@@ -34,9 +41,37 @@ describe('Wallet NftCard', () => {
 	it('should render nft price', () => {
 		const nft = makeNft()
 
-		const component = mount(NftCard, { props: { nft } })
+		const component = mount(NftCard, {
+			props: { nft, onDeletePress: () => undefined },
+		})
 		const nftPrice = component.find('[data-testid="nft-price"]')
 
 		expect(nftPrice.exists()).toBe(true)
+	})
+
+	it('should render trash can icon', () => {
+		const nft = makeNft()
+
+		const component = mount(NftCard, {
+			props: { nft, onDeletePress: () => undefined },
+		})
+		const trashCan = component.find('[data-testid="trash-can-btn"]')
+
+		expect(trashCan.exists()).toBe(true)
+	})
+
+	it('should call onDeletePress when trash can button is pressed', () => {
+		const nft = makeNft()
+		const cb = vi.fn((nft: Nft) => nft)
+
+		const component = mount(NftCard, {
+			props: { nft, onDeletePress: cb },
+		})
+		const trashCan = component.find('[data-testid="trash-can-btn"]')
+
+		trashCan.trigger('click')
+
+		expect(cb).toBeCalledTimes(1)
+		expect(cb).toBeCalledWith(nft)
 	})
 })
